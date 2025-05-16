d3d75f50e56978e586420e4d936b22f1f0174b74
trongdat01 <trongdat04012004@gmail.com>
2025-05-17 00:42:28 +0700

    fix2 API User

--- src/middlewares/validBodyRequest.js
+++ src/middlewares/validBodyRequest.js
@@ -1,3 +1,8 @@
+/**
+ * Middleware để xác thực request body theo schema
+ * @param {Object} schema - Schema Joi để xác thực
+ * @returns {Function} Middleware Express
+ */
 const validBodyRequest = (schema) => (req, res, next) => {
 	const { error, value } = schema.validate(req.body);
 
@@ -10,6 +15,33 @@ const validBodyRequest = (schema) => (req, res, next) => {
 
 	req.body = value;
 	next();
+};
+
+/**
+ * Middleware để xác thực bất kỳ thuộc tính nào của request theo schema
+ * @param {Object} schema - Schema Joi để xác thực
+ * @param {String} property - Thuộc tính của request (body, query, params...)
+ * @returns {Function} Middleware Express
+ */
+export const validateSchema = (schema, property = 'body') => {
+	return (req, res, next) => {
+		const { error, value } = schema.validate(req[property], {
+			abortEarly: false,
+			stripUnknown: true,
+			allowUnknown: true
+		});
+
+		if (error) {
+			const errorMessage = error.details.map(detail => detail.message).join(', ');
+			return res.status(400).json({
+				success: false,
+				message: errorMessage
+			});
+		}
+
+		req[property] = value;
+		next();
+	};
 };
 
 export default validBodyRequest;
